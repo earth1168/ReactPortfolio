c03d0ac6ae2e08cf286005b665d118cfd13a4f0d
Earth <pimanus1234@gmail.com>
2021-12-04 21:38:25 +0700

    Create Hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,10 +4,22 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hello there!<br />
+        My name is Pimanus Kutragoon
+
+      </SectionTitle>
+      <SectionText>
+        For me, coding is like a magic. One day, I want to use this magic to make this world a better place!
+      </SectionText>
+      <Button onClick={() => window.location = 'https://google.com'}>
+        Contact Me
+      </Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
